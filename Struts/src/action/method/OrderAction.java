040d88858a358fa10735b249b2512d1464731a75
amning <amning@qq.com>
2017-06-29 21:48:50 +0800

    struts2表单数据获取方式

---
+++ Struts/src/action/method/OrderAction.java
@@ -0,0 +1,13 @@
+package action.method;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ * Created by 王宁 on 2017/6/29.
+ */
+public class OrderAction extends ActionSupport {
+    @Override
+    public String execute() throws Exception {
+        return SUCCESS;
+    }
+}
